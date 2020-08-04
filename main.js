a00e63c30e294b216b3d9d247b961811731ed055
olawale afuye <walosha@yahoo.com>
2020-08-04 23:43:53 +0100

    added smooth scrool and night-mode

--- main.js
+++ main.js
@@ -3,4 +3,24 @@ function toggleMode() {
   element.classList.toggle("dark-mode");
 }
 
-document.querySelector("body").addEventListener("click", toggleMode);
+const links = document.querySelectorAll(".nav .nav-link");
+
+for (const link of links) {
+  link.addEventListener("click", smoothScroll);
+}
+
+function smoothScroll(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  if (href && href.startsWith("#")) {
+    const offsetTop = document.querySelector(href).offsetTop;
+    scroll({
+      top: offsetTop,
+      behavior: "smooth",
+    });
+    return;
+  }
+}
+
+//Handler for Night mode
+document.querySelector("input").addEventListener("change", toggleMode);
